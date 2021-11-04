c76baca27b00ec2e8f9c4784335f2d136bb7b6bc
bitfriend <bitfriend@hotmail.com>
2021-11-04 14:51:50 -0600

    Implement list component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
+import React, { Fragment, useEffect, useState } from 'react';
+import {
+  Avatar,
+  Divider,
+  List,
+  ListItem,
+  ListItemAvatar,
+  ListItemText,
+  Typography,
+  makeStyles
+} from '@material-ui/core';
+import moment from 'moment';
+
 import './App.css';
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: '100%',
+    backgroundColor: theme.palette.background.paper
+  }
+}));
+
 function App() {
+  const classes = useStyles();
+
+  const [records, setRecords] = useState([]);
+
+  useEffect(() => {
+    fetch('http://private-80e5dc-messages36.apiary-mock.com/messages')
+      .then(res => res.json())
+      .then(result => {
+        console.log(result);
+        setRecords(result.data);
+      });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <List className={classes.root}>
+        {records.map((record, index) => (
+          <Fragment key={index}>
+            <ListItem alignItems="flex-start">
+              <ListItemAvatar>
+                <Avatar
+                  alt={record.attributes.username}
+                  src={record.attributes.pic}
+                />
+              </ListItemAvatar>
+              <ListItemText
+                primary={(
+                  <Fragment>
+                    <Typography color="textPrimary">
+                      {record.attributes.message}
+                    </Typography>
+                    <Typography color="textPrimary">
+                      {record.attributes.username}
+                    </Typography>
+                    <Typography color="textPrimary">
+                      {record.attributes.useragent}
+                    </Typography>
+                  </Fragment>
+                )}
+                secondary={moment(record.attributes.date).format('lll')}
+              />
+            </ListItem>
+            <Divider variant="inset" component="li" />
+          </Fragment>
+        ))}
+      </List>
     </div>
   );
 }
